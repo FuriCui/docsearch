afbf1f33a154b4cd175c754758f25b10a36caa95
FuriCui <114553431+FuriCui@users.noreply.github.com>
2022-10-30 13:18:57 -0700

    1

---
+++ DocSearchTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class DocSearchTest{
+    @Test
+    public void testHandleRequest() throws Exception{
+        Handler web = new Handler ("./technical/")
+        URI ur1 = new URI ()
+    }
+}
